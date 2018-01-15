1be45e180b56da54fef4a832fed914681606f22a
adadesions <adadesions@gmail.com>
2018-01-15 13:55:32 +0700

    setup urls path for managementApp

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [    
     path('grappelli/', include('grappelli.urls')),
+    path('manage/', include('managementApp.urls')),
     path('admin/', admin.site.urls),
 ]
