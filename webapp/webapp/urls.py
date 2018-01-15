e9a2f4e0d970e3455696124ae0872a711fcca1d6
adadesions <adadesions@gmail.com>
2018-01-15 14:31:02 +0700

    Change urls managementApp from 'manage' to '/'

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [    
+urlpatterns = [   
+    path('', include('managementApp.urls')),
     path('grappelli/', include('grappelli.urls')),
-    path('manage/', include('managementApp.urls')),
     path('admin/', admin.site.urls),
 ]
