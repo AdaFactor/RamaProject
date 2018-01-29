1696d3589f6ee251f18989ab6d60da1c7a3d6676
adadesions <adadesions@gmail.com>
2018-01-29 10:38:59 +0700

    Latest

--- webapp/managementApp/urls.py
+++ webapp/managementApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing')
+    path('', views.landing, name='landing'),
+    path('demo/', views.demo, name='demo'),
 ]
--- webapp/managementApp/views.py
+++ webapp/managementApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'managementApp/index.html')
 
+
 def landing(request):
     return render(request, 'managementApp/landing.html')
+
+
+def demo(request):
+    return render(request, 'managementApp/demo_tree.html')
