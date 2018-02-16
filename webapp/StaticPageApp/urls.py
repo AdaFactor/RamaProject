3e60cbd35d3909cf932a75118b7f0f1098691e0c
adadesions <adadesions@gmail.com>
2018-02-16 13:34:34 +0700

    Add app_name

--- webapp/StaticPageApp/urls.py
+++ webapp/StaticPageApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'StaticPageApp'
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('demo/', views.demo, name='demo'),
