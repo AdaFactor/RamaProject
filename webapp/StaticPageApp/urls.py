cd15d961586ca34f6378a5bb6b89c3e031aca88a
sirimainson <sirimainson@gmail.com>
2018-02-19 16:30:01 +0700

    add admin menu page

--- webapp/StaticPageApp/urls.py
+++ webapp/StaticPageApp/urls.py
@@ -5,4 +5,6 @@ app_name = 'StaticPageApp'
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('demo/', views.demo, name='demo'),
+    path('adminmenu/', views.adminmenu, name='adminmenu'),
+    
 ]
