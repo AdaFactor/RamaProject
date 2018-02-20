4af108121fc82e7de6155a44f69bf8f2f166fc8a
sirimainson <sirimainson@gmail.com>
2018-02-21 01:22:38 +0700

    fix tree diagram

--- webapp/MemberApp/views.py
+++ webapp/MemberApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def profile(request):
     return render(request, 'MemberApp/profile.html')
+
+# def diagram(request):
+#     return render(request, 'MemberApp/diagram.html')
