2c5196867521aed3375a7faf2a1bc8f10622590b
adadesions <adadesions@gmail.com>
2018-03-06 01:02:50 +0700

    Fix static files

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from StaticPageApp.views import demo
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [   
     path('', include('StaticPageApp.urls')),
@@ -26,4 +24,4 @@ urlpatterns = [
     path('dataprocessing/', include('DataProcessingApp.urls')),
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
